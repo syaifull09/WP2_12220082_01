6801156ce861e165bc27837ff602c96715b6b1bf
syaifull09 <msyaiful093@gmail.com>
2023-10-17 19:20:15 +0700

    push pertemuan 4

---
+++ application/controllers/web.php
@@ -0,0 +1,27 @@
+<?php
+class web extends CI_Controller
+{
+    function __construct()
+    {
+        parent ::__construct();
+    }
+
+    public function index()
+
+    {
+        $data['judul']="Halaman Depan";
+        $this->load->view('v_header.php',$data);
+        $this->load->view('v_index.php',$data);
+        $this->load->view('v_footer.php',$data);
+
+    }
+    public function about()
+    {
+        $data['judul']="Halaman About";
+        $this->load->view('v_header.php',$data);
+        $this->load->view('v_about.php',$data);
+        $this->load->view('v_footer.php',$data);
+
+    }
+}
+
---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section> 
+ 	<h1><?php echo $judul ?></h1> 
+ 	<h4>Nama</h4> 
+ 	<ul type="disc"> 
+ 	 	<li>Nama Depan : Oki</li> 
+ 	 	<li>Nama Belakang : Syahputra</li> 
+ 	</ul> 
+ 	<br> 
+ 	<h4>Alamat</h4> 
+ 	<ul type="none"> 
+ 	 	<li> Jalan Bantul No.17 </li> 
+ 	</ul> 
+ 	 
+ 	<h4>Tempat Lahir</h4> 
+ 	<ul type="none"> 
+ 	 	<li>Bantul</li> 
+</ul> 
+ 
+ 	<h4>Olah Raga Favorit</h4> 
+ 	<ul type="square"> 
+ 	 	<li>Advanture</li> 
+ 	 	<li>Lari</li> 
+ 	</ul> 
+</section> 
+ 
